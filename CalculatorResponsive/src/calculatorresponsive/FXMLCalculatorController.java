d5f6fb62130427e7cc782f7e74da4b300ba94001
sofpanos <sofpanos@hotmail.com>
2018-11-29 00:43:21 +0200

    Αφαίρεση πλεονασμών και μέρος του εγγράφου

--- CalculatorResponsive/src/calculatorresponsive/FXMLCalculatorController.java
+++ CalculatorResponsive/src/calculatorresponsive/FXMLCalculatorController.java
@@ -7,8 +7,6 @@ package calculatorresponsive;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
